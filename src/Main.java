d55f58f6972cd49e242ace10e252c1d09dacfa07
IonelMoisan <46634207+IonelMoisan@users.noreply.github.com>
2023-02-22 09:17:16 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,85 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+
+public class Main {
+
+    private static final String CipherMode = "AES/ECB/NoPadding";
+
+    public static byte[] decrypt(byte[] content, String password) {
+        try {
+            SecretKeySpec key = createKey(password);
+            Cipher cipher = Cipher.getInstance(CipherMode);
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] result = cipher.doFinal(content);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String decrypt(String content, String password) {
+        byte[] data = null;
+        try {
+            data = hex2byte(content);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        data = decrypt(data, password);
+        if (data == null)
+            return null;
+        String result = null;
+        try {
+            result = new String(data, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static SecretKeySpec createKey(String password) {
+        byte[] data = null;
+        if (password == null) {
+            password = "";
+        }
+        StringBuffer sb = new StringBuffer(16);
+        sb.append(password);
+        while (sb.length() < 16) {
+            sb.append("0");
+        }
+        if (sb.length() > 16) {
+            sb.setLength(16);
+        }
+        try {
+            data = sb.toString().getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return new SecretKeySpec(data, "AES");
+    }
+
+    private static byte[] hex2byte(String inputString) {
+        if (inputString == null || inputString.length() < 2) {
+            return new byte[0];
+        }
+        inputString = inputString.toLowerCase();
+        int l = inputString.length() / 2;
+        byte[] result = new byte[l];
+        for (int i = 0; i < l; ++i) {
+            String tmp = inputString.substring(2 * i, 2 * i + 2);
+            result[i] = (byte) (Integer.parseInt(tmp, 16) & 0xFF);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                decrypt("50315FC40A9F9F9CB355531B6C171B805922A9E7D4A72F7AA4A74E6ADA5EC68B", "Th1$!sTh3P@ss<>?" )
+        );
+
+
+    }
+
+
+}
